76f76753fdd1f513b9dfda4a77cfe8cd4a7dcdf8
rasik <ahamed.rasik@gmail.com>
2021-08-08 22:12:54 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import React from "react";
+import React,{useState} from "react";
+import axios from 'axios';
 import { hot } from "react-hot-loader";
 import AlbumList from "./album/AlbumList";
 import "./App.css";
 
-const App = () => (
+const [albumList,setAlbumList] = useState([]);
+
+axios.get('https://jsonplaceholder.typicode.com/albums')
+    .then(res => setAlbumList(res.data))
+    .catch(error => alert(error))
+const App = () => (    
     <div className="albums-app">
         <h1>React Interview Challenge</h1>
-        <AlbumList />
+        <AlbumList albumList={albumList}/>
     </div>
 );
 
